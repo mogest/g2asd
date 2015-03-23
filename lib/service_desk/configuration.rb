84af0e1506356f491ad5156d34778315700044bf
Roger Nesbitt <roger@seriousorange.com>
2015-03-24 10:42:38 +1300

    Add some defaults to Configuration

--- lib/service_desk/configuration.rb
+++ lib/service_desk/configuration.rb
@@ -1,6 +1,11 @@
 class ServiceDesk::Configuration
   OPTIONS = [:client_id, :client_secret, :redirect_uri, :api_host, :authenticate_path, :token_storage, :refresh_token]
 
+  def initialize
+    api_host          "https://deskapi.gotoassist.com"
+    authenticate_path "/v2/authenticate/oauth2"
+  end
+
   OPTIONS.each do |option|
     define_method(option) do |value=nil|
       if value
